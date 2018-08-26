6ec95421ed19f6f5cfb9f4a88e9b30bc892672bc
ThomasHere123 <thom.palts@gmail.com>
2018-08-26 02:11:55 +0200

    Basics
    Added main, setup pom.xml (maven), startup message and setup infrastructure for later development.

---
+++ MTJavaFails/src/main/java/utilities/starthere.java
@@ -0,0 +1,12 @@
+package utilities;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class starthere extends JavaPlugin {
+	
+	public void onEnable() {
+		getLogger().info("Hey. Lets hope life doesnt crash.");
+	}
+
+}
