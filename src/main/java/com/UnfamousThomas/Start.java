767b6e93b01eba68e72cfa13a232dbdb810b98ad
UnfamousThomas <thompalts@gmail.com>
2018-08-26 20:21:03 +0300

    Change to IDEA INTELLIJ BC IT IS SO MUCH EASIER OMG

---
+++ src/main/java/com/UnfamousThomas/Start.java
@@ -0,0 +1,11 @@
+package com.UnfamousThomas;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Start extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getLogger().info("WOW IT ENABLED!");
+    }}
